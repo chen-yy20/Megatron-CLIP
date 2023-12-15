374967244211ad8d3e940fa8746ac2dbeee289a4
yuyang <892016730@qq.com>
2023-12-15 10:36:37 +0800

    merge (both situation successful)

--- run_CLIP.sh
+++ run_CLIP.sh
@@ -21,8 +21,8 @@ export MODEL_NAME='CLIP'
 # export DATA_PARALLEL_SIZE='2'
 export GPUS_PER_NODE='8'
 # export NNODES=$(( $DATA_PARALLEL_SIZE * $TENSOR_PARALLEL_SIZE * $PIPELINE_PARALLEL_SIZE / $GPUS_PER_NODE))
-export NNODES='4'
-export NODELIST='nico[1-4]'
+export NNODES='2'
+export NODELIST='nico[2,4]'
 
 # export GLOBAL_BATCH_SIZE='64'
 # export MICRO_BATCH_SIZE='4'
